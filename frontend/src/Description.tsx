94ea56b3c296252a4a1a31b50531601e3c80f2a8
kitkatglider <159606642+kitkatglider@users.noreply.github.com>
2024-09-25 01:01:54 +0530

    styled description page

--- frontend/src/Description.tsx
+++ frontend/src/Description.tsx
@@ -12,13 +12,15 @@ export default function Description(props: any) {
 function Detail(_descriptionId: any) {
   const response = fetch
   return( 
+  <div>
   <div className="container">
   <Picture image={"/goldenretriver.webp"} desc={"goldenretriver"} />
   <h2 className="breed-name">Golden Retriever</h2>
+  </div>
   <div className="details">
-    <p><span>Age Limit:</span> 10-12 years</p>
-    <p><span>Characteristics:</span> Friendly, intelligent, and devoted</p>
-    <p><span>Food:</span> High-quality dry food and occasional wet food</p>
+    <p><span className="heading">Age Limit:</span> 10-12 years</p>
+    <p><span className="heading">Characteristics:</span> Friendly, intelligent, and devoted</p>
+    <p><span className="heading">Food:</span> High-quality dry food and occasional wet food</p>
   </div>
 </div>
   )  
